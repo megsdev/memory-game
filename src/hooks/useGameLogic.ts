37db1969e2c5e4fa4e023dc447b879c54d0c0171
megsdev <megduclos@gmail.com>
2023-05-04 11:44:58 -0600

    add hooks

---
+++ src/hooks/useGameLogic.ts
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import { CardWithId, Gif } from 'src/common/types'
+import { addUniqueIds, buildCards, duplicateCards, shuffleCards } from '../common/utils'
+
+const useGameLogic = (gifs: Gif[], difficulty: string) => {
+const [cards, setCards] = useState<CardWithId[]>([])
+const [visibleCards, setVisibleCards] = useState<CardWithId[]>([])
+const [score, setScore] = useState(0);
+const [isWin, setIsWin] = useState(false);
+
+const MAX_VISIBLE_CARDS = 2;
+const PACES: object = {
+    easy: 1500,
+    medium: 1000,
+    hard: 500,
+    pro: 200,
+}
+
+const prepareCards = () => {
+const initialCards = buildCards(gifs)
+const duplicatedCards = duplicateCards(initialCards)
+const cardsWithUniqueIds = addUniqueIds(duplicatedCards)
+const shuffledCards = shuffleCards(cardsWithUniqueIds)
+
+setCards(shuffledCards)
+}
+
+const flipCard = (clickedCardId: number) => {
+    const flippedCards = cards?.map(card => {
+        if(card.uniqueId === clickedCardId) {
+            card.isShown = true
+        }
+
+        if(card.isShown) setVisibleCards(prevState => [...prevState, card])
+
+        return card
+    })
+    
+    setCards(flippedCards)
+}
+
+const onCardClick = (clickedCardId: number)=> {
+    if(visibleCards?.length < MAX_VISIBLE_CARDS) {
+
+        flipCard(clickedCardId)
+    }
+}
+
+const updateScore = () => {
+    setScore(oldScore => oldScore +1)
+}
+
+const checkMatch = () => {
+    const visible = cards.filter(card => visibleCards.indexOf(card) !== -1)
+    const matched = visible[0].id === visible[1].id
+
+    const updatedCards = cards.map(card => {
+        if (visibleCards.indexOf(card) !== -1) {
+            card.isShown = false
+            card.isFound = matched
+        }
+        return card
+    })
+
+    setTimeout(() => {
+        setCards(updatedCards)
+        setVisibleCards([])
+        if (matched) updateScore()
+        
+    }, PACES[difficulty as keyof object]);
+}
+
+useEffect(() => {
+    if (gifs.length > 0) prepareCards()
+}, [gifs]) 
+
+useEffect(() => {
+    if (visibleCards?.length >= MAX_VISIBLE_CARDS) {
+        checkMatch()
+    }
+}, [visibleCards])
+
+useEffect(() => {
+    if (gifs.length && score === gifs.length) {
+        setIsWin(true)
+    }
+})
+
+return {cards, onCardClick, isWin}
+}
+
+export default useGameLogic
---
+++ src/hooks/useGetGifs.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+import { GameSettings, Gif } from '../common/types'
+
+
+const getRandomOffset = (): number => Math.round(Math.random() * (100 - 1) + 1)
+
+const useGetGifs = (gameSettings: GameSettings): Gif[] => {
+    const [gifs, setGifs] = useState<Gif[]>([])
+    const buildUrl = () => {
+        let url = new URL('https://api.giphy.com/v1/gifs/search')
+        const limit = gameSettings.deckSize / 2
+        url.search = new URLSearchParams({
+          api_key: process.env.REACT_APP_AUTH_KEY ?? '',
+          q: `taylor+swift`,
+          limit: limit.toString(),
+          offset: getRandomOffset().toString(), 
+          rating: 'pg-13',
+          lang: 'en'
+      }).toString()
+    
+      return url
+    }
+    
+    const fetchGifs = () => {
+      fetch(buildUrl()).then(data => data.json())
+      .then(data => setGifs(data.data))
+    }
+
+    useEffect(() => {
+      if(!gameSettings) return
+    fetchGifs()
+    }, [gameSettings])
+
+    return gifs;
+}
+
+export default useGetGifs
